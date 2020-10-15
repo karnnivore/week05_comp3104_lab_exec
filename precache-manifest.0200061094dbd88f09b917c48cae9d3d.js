304d3568e12ffd010e51c892276cc72c18100831
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-15 00:48:57 +0000

    Deploy karnnivore/week05_comp3104_lab_exec to github.com/karnnivore/week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.0200061094dbd88f09b917c48cae9d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ec46fbb3b24bbdd7c22",
+    "url": "/static/css/main.279b8a0f.chunk.css"
+  },
+  {
+    "revision": "5ec46fbb3b24bbdd7c22",
+    "url": "/static/js/main.5ec46fbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "19345ebdfbf20effd286daa820db532a",
+    "url": "/index.html"
+  }
+];
